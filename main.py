fc7213693e37c58d19f527e122d3244e47f90a7c
Arjun <arjunai5768@gmail.com>
2024-02-21 00:17:16 +0530

    Gender added

--- main.py
+++ main.py
@@ -20,51 +20,37 @@ while(count < 25):
         # submit.click()
         
         # # Wait for the radio button to be clickable, then click
-        # radio = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[32]/div/div/div[2]/div/span/div/label[1]/div[2]/div/div/div[3]/div')))
-        # radio.click()
-        
-        # radio = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[32]/div/div/div[2]/div/span/div/label[2]/div[2]/div/div/div[3]/div')))
-        # radio.click()
-        
-        # //*[@id="mG61Hd"]/div[2]/div/div[2]/div[31]/div/div/div[2]/div/span/div/label[1]/div[2]/div/div/div[3]/div
+        gender_opt=random.choice([5,8,11])
+      
+        gen_path=   f'//*[@id="i{gender_opt}"]/div[3]/div'
+        gender = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="i5"]/div[3]/div')))
+        gender.click()
 
-        # radio = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="i5"]/div[3]/div')))
-        # radio.click()
         
         
         q_count_1=8
         while(q_count_1<24):
-            # opt = random.randint(1,5)
-            # xpath=f'//*[@id="mG61Hd"]/div[2]/div/div[2]/div[{q_count}]]/div/div/div[2]/div/span/div/label[{opt}]/div[2]/div/div/div[3]/div'
-            # radio = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
-            # radio.click()
-        
             opt = random.randint(1, 5)
             radio_xpath = f'//*[@id="mG61Hd"]/div[2]/div/div[2]/div[{q_count_1}]/div/div/div[2]/div/span/div/label[{opt}]/div[2]/div/div/div[3]/div'
             radio = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, radio_xpath)))
             radio.click()
             q_count_1=q_count_1+1
-
-            # //*[@id="mG61Hd"]/div[2]/div/div[2]/div[8]/div/div/div[2]/div/span/div/label[2]/div[2]/div/div/div[3]/div
-            # //*[@id="mG61Hd"]/div[2]/div/div[2]/div[23]/div/div/div[2]/div/span/div/label[3]/div[2]/div/div/div[3]/div
-
-
-            # //*[@id="mG61Hd"]/div[2]/div/div[2]/div[25]/div/div/div[2]/div/span/div/label[2]/div[2]/div/div/div[3]/div/div
-            # //*[@id="mG61Hd"]/div[2]/div/div[2]/div[28]/div/div/div[2]/div/span/div/label[1]/div[2]/div/div/div[3]/div
-        
-        
+    
         q_count_2=25
-        
         while(q_count_2<29):
             opt = random.randint(1, 5)
             radio_xpath = f'//*[@id="mG61Hd"]/div[2]/div/div[2]/div[{q_count_2}]/div/div/div[2]/div/span/div/label[{opt}]/div[2]/div/div/div[3]/div'
             radio = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, radio_xpath)))
             radio.click()
-            q_count_2=q_count_2+1
-            
+            q_count_2=q_count_2+1        
         
-        
-        
+        q_count_3=30
+        while(q_count_3<35):
+            opt = random.randint(1, 5)
+            radio_xpath = f'//*[@id="mG61Hd"]/div[2]/div/div[2]/div[{q_count_3}]/div/div/div[2]/div/span/div/label[{opt}]/div[2]/div/div/div[3]/div'
+            radio = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, radio_xpath)))
+            radio.click()
+            q_count_3=q_count_3+1
         
         
         
